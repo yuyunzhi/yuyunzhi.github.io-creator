48deb6ad41d5fb403ff32cc67f10dd9e96ae6ff0
lambert1129 <lambert1129@foxmail.com>
2019-11-29 10:11:22 +0800

    shell提交：

--- git.sh
+++ git.sh
@@ -9,11 +9,5 @@ git commit -m "shell提交：$a"
 
 echo "需要push到：b-someone-edit分支则输入y"
 
-read choose
+git push
 
-if [ $choose = "y" ];then
-echo "正在执行命令：git push origin b-someone-edit "
-git push origin b-someone-edit
-else
-echo "您没有push到服务器"
-fi
