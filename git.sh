2a3d3e30b5681299ddfabfede4c4ff4e57ce1e6d
lambert1129 <lambert1129@foxmail.com>
2019-11-29 11:32:29 +0800

    update:shell分割线

--- git.sh
+++ git.sh
@@ -5,13 +5,16 @@ cd /Users/yuyunzhi/Desktop/yuyunzhi.github.io
 # 生成静态文件
 hugo
 
+echo "-对整个项目文件进行推送github---------------------------------"
 # 对整个项目文件进行推送github
 git status
 git add .
-echo "---github.io-creator----输入您的commit文字"
+echo "输入您的commit文字"
 read a
 git commit -m "$a"
 git push
+
+echo "-对public文件进行推送github---------------------------------"
 
 # 对public文件进行推送github
 cd /Users/yuyunzhi/Desktop/yuyunzhi.github.io/public
