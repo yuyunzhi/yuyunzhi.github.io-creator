0cf5ffd3d34d19136945fc4143184c5aa649447f
lambert1129 <lambert1129@foxmail.com>
2020-02-11 20:33:43 +0800

    添加socketio

---
+++ static/js/socket-io.js
@@ -0,0 +1,55 @@
+import SocketIO from 'socket.io-client'
+
+// 心跳检测
+const heartCheck = (socket, userId) => {
+    let timeout = 0
+
+    window.setInterval(() => {
+        if (timeout <= 0) {
+            socket.emit('refresh', { userId })
+            timeout = 30
+        } else {
+            timeout--
+        }
+    }, 1000)
+}
+
+function SocketIOClient (url, userId, token) {
+    const socket = SocketIO(url, {
+        transports: ['websocket', 'polling', 'flashsocket']
+    })
+
+    this.url = url
+    this.userId = userId
+    this.token = token
+    this.socket = socket
+}
+
+// 打开连接&发送握手事件
+SocketIOClient.prototype.openConnect = function () {
+    const socket = this.socket
+    const userId = this.userId
+    const token = this.token
+
+    socket.on('connect', () => {
+        heartCheck(socket, userId)
+
+        // 发送握手事件
+        socket.emit('handshake', { token: userId, auth: token }, res => {
+            res = res || {}
+        })
+    })
+}
+
+// 接收消息
+SocketIOClient.prototype.receive = function (store) {
+    const socket = this.socket
+
+    socket.on('notification', ({ payload }) => {
+        if (!payload) return
+
+        store.dispatch('updateSocketData', payload)
+    })
+}
+
+export default SocketIOClient
